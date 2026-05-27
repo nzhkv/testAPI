33049fceae9eb2c4b98609db5c8311066cb9b095
nzhukov <nzhukov.rnd@gmail.com>
2026-05-27 19:10:03 +0400

    Initial Commit

---
+++ AutodocZhukovTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutodocZhukovTest
+//
+//  Created by Nikolai Zhukov on 27/05/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
